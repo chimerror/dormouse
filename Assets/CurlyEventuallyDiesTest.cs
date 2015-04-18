062b1f8223561b34208b1235e974fb021326a6ff
foobardog <foobardog@hotmail.com>
2015-04-17 19:25:30 -0700

    Simple Town Scene and basic Integration test seeing that people eventually die.

---
+++ Assets/CurlyEventuallyDiesTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[IntegrationTest.DynamicTest("TownIntegrationTests")]
+[IntegrationTest.Timeout(60)]
+public class CurlyEventuallyDiesTest : MonoBehaviour
+{
+    private GameObject _target;
+    private PersonBehaviour _targetBehaviour;
+
+    void Start ()
+    {
+        _target = GameObject.FindGameObjectsWithTag("Person").First(p => p.name == "Curly");
+        _targetBehaviour = _target.GetComponent<PersonBehaviour>();
+    }
+
+    void Update ()
+    {
+        if (!_targetBehaviour.Alive)
+        {
+            IntegrationTest.Pass();
+        }
+    }
+}
---
+++ Assets/PersonBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonBehaviour : MonoBehaviour
+{
+    private GameObject _goal;
+
+    public bool Alive = true;
+    public float Hunger = 0.75f;
+    public float HungerRate = -0.01f;
+    public float Thirst = 0.75f;
+    public float ThirstRate = -0.05f;
+    public float Sanity = 0.75f;
+    public float SanityRate = -0.07f;
+    public float Energy = 1.0f;
+    public float EnergyRate = -0.01f;
+    public float Health = 1.0f;
+    public float HealthRate = -0.1f;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (!Alive)
+        {
+            return;
+        }
+
+        float delta = Time.fixedDeltaTime;
+        Hunger = AdjustStatistic(delta, HungerRate, Hunger);
+        Thirst = AdjustStatistic(delta, ThirstRate, Thirst);
+
+        if (Hunger <= 0.0f || Thirst <= 0.0f)
+        {
+            Health = AdjustStatistic(delta, HealthRate, Health);
+        }
+
+        if (Health <= 0.0f)
+        {
+            GetComponent<SpriteRenderer>().color = Color.black;
+            Alive = false;
+            Debug.LogFormat("Person {0} died!", name);
+        }
+    }
+
+    private float AdjustStatistic(float delta, float rate, float currentValue)
+    {
+        float nextValue = currentValue + (rate * delta);
+        if (nextValue < 0.0f)
+        {
+            nextValue = 0.0f;
+        }
+        else if (nextValue > 1.0f)
+        {
+            nextValue = 1.0f;
+        }
+
+        return nextValue;
+    }
+}
