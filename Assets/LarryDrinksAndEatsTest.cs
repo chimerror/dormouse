7f889518fb977600af8e1db12ec3e109ff2dabc8
foobardog <foobardog@hotmail.com>
2015-04-17 21:41:33 -0700

    Added behavior to seek food and water with associated test.

---
+++ Assets/LarryDrinksAndEatsTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[IntegrationTest.DynamicTest("TownIntegrationTests")]
+[IntegrationTest.Timeout(60)]
+public class LarryDrinksAndEatsTest : MonoBehaviour
+{
+    private GameObject _target;
+    private PersonBehaviour _targetBehaviour;
+    private bool _hasDrank = false;
+    private bool _hasEaten = false;
+
+    void Start()
+    {
+        _target = GameObject.FindGameObjectsWithTag("Person").First(p => p.name == "Larry");
+        _targetBehaviour = _target.GetComponent<PersonBehaviour>();
+    }
+
+    void Update()
+    {
+        if (_targetBehaviour.Drinking)
+        {
+            _hasDrank = true;
+        }
+
+        if (_targetBehaviour.Eating)
+        {
+            _hasEaten = true;
+        }
+
+        if (_hasDrank && _hasEaten)
+        {
+            IntegrationTest.Pass();
+        }
+    }
+}
--- Assets/PersonBehaviour.cs
+++ Assets/PersonBehaviour.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class PersonBehaviour : MonoBehaviour
 {
-    private GameObject _goal;
-
+    public GameObject _goal;
+    public bool MoveToGoal = false;
     public bool Alive = true;
+    public bool Eating = false;
     public float Hunger = 0.75f;
     public float HungerRate = -0.01f;
+    public bool Drinking = false;
     public float Thirst = 0.75f;
     public float ThirstRate = -0.05f;
     public float Sanity = 0.75f;
@@ -16,6 +18,7 @@ public class PersonBehaviour : MonoBehaviour
     public float EnergyRate = -0.01f;
     public float Health = 1.0f;
     public float HealthRate = -0.1f;
+    public float Velocity = 0.0f;
 
     void Start()
     {
@@ -29,8 +32,32 @@ public class PersonBehaviour : MonoBehaviour
         }
 
         float delta = Time.fixedDeltaTime;
-        Hunger = AdjustStatistic(delta, HungerRate, Hunger);
-        Thirst = AdjustStatistic(delta, ThirstRate, Thirst);
+        Hunger = AdjustStatistic(delta, Eating ? 0.1f : HungerRate, Hunger);
+        Thirst = AdjustStatistic(delta, Drinking ? 0.1f : ThirstRate, Thirst);
+
+        if (_goal == null && !Drinking && Thirst <= 0.4f)
+        {
+            _goal = GameObject.FindGameObjectWithTag("Water");
+            MoveToGoal = true;
+        }
+        else if (Drinking && Thirst >= 1.0f)
+        {
+            Drinking = false;
+            _goal = null;
+            MoveToGoal = false;
+        }
+
+        if (_goal == null && !Eating && Hunger <= 0.4f)
+        {
+            _goal = GameObject.FindGameObjectWithTag("Food");
+            MoveToGoal = true;
+        }
+        else if (Eating && Hunger >= 1.0f)
+        {
+            Eating = false;
+            _goal = null;
+            MoveToGoal = false;
+        }
 
         if (Hunger <= 0.0f || Thirst <= 0.0f)
         {
@@ -43,11 +70,36 @@ public class PersonBehaviour : MonoBehaviour
             Alive = false;
             Debug.LogFormat("Person {0} died!", name);
         }
+
+        if (_goal != null && MoveToGoal)
+        {
+            transform.position += (_goal.transform.position - transform.position) * (Velocity * delta);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject == _goal)
+        {
+            MoveToGoal = false;
+
+            if (collider.gameObject.tag.Equals("Water"))
+            {
+                Drinking = true;
+            }
+
+            if (collider.gameObject.tag.Equals("Food"))
+            {
+                Debug.LogFormat("Started eating at: {0}", Time.fixedTime);
+                Eating = true;
+            }
+        }
     }
 
     private float AdjustStatistic(float delta, float rate, float currentValue)
     {
         float nextValue = currentValue + (rate * delta);
+
         if (nextValue < 0.0f)
         {
             nextValue = 0.0f;
